fe32736469b7ebeed849d2c091ddd377ad1974fe
Xiaotian <BilibiliXiaotian@outlook.com>
2026-03-03 14:55:57 +0800

    fix: debug

--- boot/boot_core.py
+++ boot/boot_core.py
@@ -35,7 +35,6 @@ def main(is_builtin: bool) -> int:
     VAR_MONITOR.watch('boot_main.main@is_builtin', is_builtin)
     init()
     server_repl()
-    0/0
     while get_server_status() and get_builtin():
         WSL.peek()
     return 0
