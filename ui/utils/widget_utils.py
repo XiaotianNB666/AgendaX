bd6e3988dcd3a5906e30264c298637332fb51d8b
Xiaotian <BilibiliXiaotian@outlook.com>
2026-03-03 20:54:10 +0800

    feat: ui completion: floating ball

---
+++ ui/utils/widget_utils.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QWidget
+
+def set_center(widget: QWidget, parent_widget: QWidget) -> None:
+    container_size = parent_widget.size()
+    widget_size = widget.size()
+    print(widget_size)
+
+    x = (container_size.width() - widget_size.width()) // 2
+    y = (container_size.height() - widget_size.height()) // 2
+
+    widget.setGeometry(x, y, widget_size.width(), widget_size.height())
